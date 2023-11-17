fd04e2e8fccf7351c20e9782a31ce2acd56558d6
kura <91504106+kura54@users.noreply.github.com>
2023-11-17 15:55:26 +0900

    Initial commit

---
+++ src/components/Emotion.jsx
@@ -0,0 +1,25 @@
+/** @JsxRuntime classic */
+/** @jsx */
+import { jsx, css } from "@emotion/react";
+
+export const Emotion = () => {
+  const containerStyle = css`
+    border: solid 2px #392eff;
+    border-radius: 20px;
+    padding: 8px;
+    margin: 8px;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+  `;
+  const titleStyle = css({
+    margin: 0,
+    color: "#3d84a8"
+  });
+  return (
+    <div css={containerStyle}>
+      <p css={titleStyle}>-- Emotion --</p>
+      <button>FIGHT!!</button>
+    </div>
+  );
+};
